2bac9acbd96f40b34fdbef42625460ab110ddb1b
Alexander Krastev <alexander@finite-soft.com>
2020-10-01 09:16:23 +0300

    added new path for axios

--- ui/src/axios.js
+++ ui/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://chessable.me'
+    baseURL: 'http://127.0.0.1'
 });
 
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
